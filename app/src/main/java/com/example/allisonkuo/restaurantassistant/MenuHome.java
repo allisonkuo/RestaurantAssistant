d1fd8bdab0fef8c4a78b1fab9fe206e79799475a
Allison Kuo <allisonkuo@Allisons-MacBook-Pro.local>
2017-02-28 13:11:53 -0800

    Initial commit

---
+++ app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
@@ -0,0 +1,13 @@
+package com.example.allisonkuo.restaurantassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_home);
+    }
+}
