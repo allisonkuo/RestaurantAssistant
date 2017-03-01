b0379ab703b0d116b7e07e2713dd6c9c2573e92f
allisonkuo <aly.kuo@gmail.com>
2017-03-01 01:12:27 -0800

    Finished adding icons for the landing page buttons

--- app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
+++ app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
