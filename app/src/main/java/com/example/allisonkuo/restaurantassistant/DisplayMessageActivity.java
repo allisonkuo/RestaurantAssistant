17c08d4531a24f7ebcc99540ab87deaf1b2812a2
allisonkuo <aly.kuo@gmail.com>
2017-02-28 22:20:53 -0800

    Styling buttons

---
+++ app/src/main/java/com/example/allisonkuo/restaurantassistant/DisplayMessageActivity.java
@@ -0,0 +1,18 @@
+package com.example.allisonkuo.restaurantassistant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
--- app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
+++ app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
@@ -1,13 +1,34 @@
 package com.example.allisonkuo.restaurantassistant;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MenuHome extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_home);
+    }
+
+    ImageButton button_menu = (ImageButton) findViewById(R.id.button_menu);
+    int btn_size = button_menu.getLayoutParams().width;
+    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(btn_size, btn_size);
+    button_menu.setLayoutParams(param);å
+
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+
     }
 }
