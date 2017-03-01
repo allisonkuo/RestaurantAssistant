780a5655ec68c9232d848d67c7590daacb36f533
allisonkuo <aly.kuo@gmail.com>
2017-03-01 12:31:41 -0800

    Added link between food menu button and food menu activity

--- app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
+++ app/src/main/java/com/example/allisonkuo/restaurantassistant/MenuHome.java
@@ -26,6 +26,7 @@ public class MenuHome extends AppCompatActivity {
 
     /** Called when the user clicks the food menu button */
     public void test(View view) {
-
+        Intent intent = new Intent(this, FoodMenuActivity.class);
+        startActivity(intent);
     }
 }
