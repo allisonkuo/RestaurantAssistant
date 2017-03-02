8bf93e5cd8e65f62a554cf3aa1ac3c1be6f1822c
allisonkuo <aly.kuo@gmail.com>
2017-03-02 00:42:57 -0800

    Menu items change to match chosen category

--- app/src/main/java/com/example/allisonkuo/restaurantassistant/FoodMenuActivity.java
+++ app/src/main/java/com/example/allisonkuo/restaurantassistant/FoodMenuActivity.java
@@ -28,12 +28,17 @@ public class FoodMenuActivity extends AppCompatActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private String mActivityTitle;
 
+    private ArrayList<String> appetizers = new ArrayList<String>();
+    private ArrayList<String> burgers = new ArrayList<String>();
+    private ArrayList<String> sandwiches = new ArrayList<String>();
+    private ArrayList<String> desserts = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_menu);
 
+        // navigation bar
         mDrawerList = (ListView) findViewById(R.id.navList);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mActivityTitle = getTitle().toString();
@@ -44,16 +49,16 @@ public class FoodMenuActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        ArrayList<String> appetizers = new ArrayList<String>();
+        // initialize arrays for different menu items
         appetizers.add("nachos");
         appetizers.add("wings");
         appetizers.add("mozarella sticks");
+        burgers.add("cheese");
+        sandwiches.add("grilled cheese");
 
         MyCustomAdapter adapter = new MyCustomAdapter(appetizers, this);
         ListView listView = (ListView) findViewById(R.id.menu_items);
         listView.setAdapter(adapter);
-
-
     }
 
     private void addDrawerItems() {
@@ -64,10 +69,33 @@ public class FoodMenuActivity extends AppCompatActivity {
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(FoodMenuActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
-                //TextView textView = (TextView) findViewById(R.id.menu_text);
-                //textView.setText("NEW TEXT");
+                //Toast.makeText(FoodMenuActivity.this, foodCategories[position], Toast.LENGTH_SHORT).show();
 
+                // populate menu items for chosen category
+                MyCustomAdapter adapter;
+                ListView listView = (ListView) findViewById(R.id.menu_items);
+
+                switch (foodCategories[position]) {
+                    case "Appetizers":
+                        adapter = new MyCustomAdapter(appetizers, FoodMenuActivity.this);
+                        listView.setAdapter(adapter);
+                        break;
+
+                    case "Burgers":
+                        adapter = new MyCustomAdapter(burgers, FoodMenuActivity.this);
+                        listView.setAdapter(adapter);
+                        break;
+
+                    case "Sandwiches":
+                        adapter = new MyCustomAdapter(sandwiches, FoodMenuActivity.this);
+                        listView.setAdapter(adapter);
+                        break;
+
+                    case "Desserts":
+                        adapter = new MyCustomAdapter(desserts, FoodMenuActivity.this);
+                        listView.setAdapter(adapter);
+                        break;
+                }
             }
         });
     }
